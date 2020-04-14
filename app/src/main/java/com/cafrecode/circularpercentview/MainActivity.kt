e11ccb3b21694b012f731f38d107d59bacc5d7d6
Frederick Nyawaya <o.frederickn@gmail.com>
2020-04-14 13:22:45 +0300

    added sample resources

--- app/src/main/java/com/cafrecode/circularpercentview/MainActivity.kt
+++ app/src/main/java/com/cafrecode/circularpercentview/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.cafrecode.circularpercentview
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-
-import kotlinx.android.synthetic.main.activity_main.*
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
-
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
     }
 }
